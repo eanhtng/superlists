4a4bf597c4e7235fae366b4b8571ebee28a563c8
AnhNT <anhnt@edu.solutions>
2020-03-04 15:40:43 +0700

    Use Bootstrap to  improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,11 +3,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import unittest
 import time
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
-MAX_WAIT = 3
+MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -110,14 +111,14 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith goes to the home page
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta =10)
 
-        # She starts a new list and sees the input is nicely centered there too
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta =10)
+            # She starts a new list and sees the input is nicely centered there too
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: testing')
+
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta =10)
