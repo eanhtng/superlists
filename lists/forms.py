eb05c474d25858d5780d2fadcd50de8898053c9b
AnhNT <anhnt@edu.solutions>
2020-03-13 21:28:39 +0700

    add list owners via forms. more isolated tests

--- lists/forms.py
+++ lists/forms.py
@@ -1,3 +1,4 @@
+from lists.models import Item, List
 from django.core.exceptions import ValidationError
 from lists.models import Item
 from django import forms
@@ -18,9 +19,7 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
         }
-    def save(self, for_list):
-        self.instance.list = for_list
-        return super().save()
+
 
 class ExistingListItemForm(ItemForm):
     def __init__(self, for_list, *args, **kwargs):
@@ -34,5 +33,12 @@ class ExistingListItemForm(ItemForm):
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
 
-    def save(self):
-        return forms.models.ModelForm.save(self)
+
+
+class NewListForm(ItemForm):
+
+    def save(self, owner):
+        if owner.is_authenticated:
+            return List.create_new(first_item_text=self.cleaned_data['text'], owner=owner)
+        else:
+            return List.create_new(first_item_text=self.cleaned_data['text'])
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
+from lists.forms import ExistingListItemForm, ItemForm, NewListForm
 from django.contrib.auth import get_user_model
 User = get_user_model()
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR, ExistingListItemForm
@@ -11,16 +12,11 @@ def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
 def new_list(request):
-    form = ItemForm(data = request.POST)
+    form = NewListForm(data=request.POST)
     if form.is_valid():
-        #list_ = List.objects.create()
-        list_=List()
-        list_.owner = request.user
-        list_.save()
-        form.save(for_list=list_)
+        list_ = form.save(owner=request.user)
         return redirect(list_)
-    else:
-        return render(request, 'home.html', {'form': form})
+    return render(request, 'home.html', {'form': form})
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
